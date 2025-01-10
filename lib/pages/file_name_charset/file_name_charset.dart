ef33f009c2c6a2c10c3dcd71901bf82507322036
ilgnefz <ilgnefz@foxmail.com>
2025-01-10 13:06:22 +0800

    添加了新功能

--- lib/pages/file_name_charset/file_name_charset.dart
+++ lib/pages/file_name_charset/file_name_charset.dart
@@ -1,5 +1,7 @@
 import 'dart:io';
 
+import 'package:cross_file/cross_file.dart';
+import 'package:desktop_drop/desktop_drop.dart';
 import 'package:disposable_tool/model/name_charset.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:fl_charset/fl_charset.dart';
@@ -17,6 +19,25 @@ class FileNameCharset extends StatefulWidget {
 class _FileNameCharsetState extends State<FileNameCharset> {
   List<NameCharset> fileList = [];
 
+  onDrag(DropDoneDetails detail) async {
+    List<XFile> files = detail.files;
+    var uuid = const Uuid();
+    for (var xFile in files) {
+      String id = uuid.v1();
+      String name = path.basename(xFile.path);
+      // String newName = shiftJis.decode(gbk.encode(name));
+      String parent = path.dirname(xFile.path);
+      NameCharset nameCharset = NameCharset(
+        id: id,
+        name: name,
+        newName: name,
+        parent: parent,
+      );
+      fileList.add(nameCharset);
+      setState(() {});
+    }
+  }
+
   void getFiles() async {
     fileList.clear();
     String? dir = await FilePicker.platform.getDirectoryPath();
@@ -25,19 +46,19 @@ class _FileNameCharsetState extends State<FileNameCharset> {
     // 获取文件夹下的所有子文件夹
     List<FileSystemEntity> parentDirList =
         await Directory(dir).list().where((e) => e is Directory).toList();
-    List<FileSystemEntity> childrenDir = [];
-    for (final parentDir in parentDirList) {
-      List<FileSystemEntity> children = await Directory(parentDir.path)
-          .list(recursive: true)
-          .where((e) => e is Directory)
-          .toList();
-      //  如果子文件夹的名称只有英文或只有数字，或只由英文和数字组成就删除
-      children = children
-          .where(
-              (e) => !RegExp(r'^[a-zA-Z0-9]+$').hasMatch(path.basename(e.path)))
-          .toList();
-      childrenDir.addAll(children);
-    }
+    List<FileSystemEntity> childrenDir = parentDirList;
+    // for (final parentDir in parentDirList) {
+    //   List<FileSystemEntity> children = await Directory(parentDir.path)
+    //       .list(recursive: true)
+    //       .where((e) => e is Directory)
+    //       .toList();
+    //   //  如果子文件夹的名称只有英文或只有数字，或只由英文和数字组成就删除
+    //   children = children
+    //       .where(
+    //           (e) => !RegExp(r'^[a-zA-Z0-9]+$').hasMatch(path.basename(e.path)))
+    //       .toList();
+    //   childrenDir.addAll(children);
+    // }
     var uuid = const Uuid();
     for (final file in childrenDir) {
       String id = uuid.v1();
@@ -54,6 +75,7 @@ class _FileNameCharsetState extends State<FileNameCharset> {
       setState(() {});
     }
     print('完成！！！');
+    print(fileList);
   }
 
   void removeFile(NameCharset nameCharset) {
@@ -120,93 +142,96 @@ class _FileNameCharsetState extends State<FileNameCharset> {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(12.0),
-      child: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(left: 12, right: 8),
-            child: Row(
+      child: DropTarget(
+        onDragDone: (detail) => onDrag(detail),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 12, right: 8),
+              child: Row(
+                children: [
+                  Expanded(child: Text('原名称 —— ${fileList.length} 个文件夹')),
+                  const SizedBox(width: 12),
+                  const Expanded(child: Text('新名称')),
+                  IconButton(
+                    onPressed: deleteAll,
+                    icon: const Icon(Icons.delete),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: SelectionArea(
+                  child: ListView.builder(
+                    itemBuilder: (context, index) {
+                      NameCharset nameCharset = fileList[index];
+                      return Container(
+                        padding: const EdgeInsets.only(
+                            left: 12, right: 8, top: 8, bottom: 8),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.max,
+                          children: [
+                            Expanded(child: Text(nameCharset.name)),
+                            const SizedBox(width: 12),
+                            Expanded(
+                              child: Text(
+                                nameCharset.newName,
+                                style: nameCharset.name != nameCharset.newName
+                                    ? TextStyle(color: Colors.blue[400])
+                                    : null,
+                              ),
+                            ),
+                            IconButton(
+                              onPressed: () => removeFile(fileList[index]),
+                              icon: const Icon(Icons.delete),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                    itemCount: fileList.length,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 12),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Expanded(child: Text('原名称 —— ${fileList.length} 个文件夹')),
+                ElevatedButton(
+                  onPressed: getFiles,
+                  child: const Text('选择文件夹'),
+                ),
                 const SizedBox(width: 12),
-                const Expanded(child: Text('新名称')),
-                IconButton(
-                  onPressed: deleteAll,
-                  icon: const Icon(Icons.delete),
+                ElevatedButton(
+                  onPressed: changeNewName,
+                  child: const Text('转变新名称'),
+                ),
+                const SizedBox(width: 12),
+                ElevatedButton(
+                  onPressed: undoChangeName,
+                  child: const Text('撤销转变'),
+                ),
+                const SizedBox(width: 12),
+                ElevatedButton(
+                  onPressed: removeSameName,
+                  child: const Text('删除同名项'),
+                ),
+                const SizedBox(width: 12),
+                ElevatedButton(
+                  onPressed: renameDir,
+                  child: const Text('确定更改'),
                 ),
               ],
             ),
-          ),
-          Expanded(
-            child: Container(
-              width: double.infinity,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(8),
-              ),
-              child: SelectionArea(
-                child: ListView.builder(
-                  itemBuilder: (context, index) {
-                    NameCharset nameCharset = fileList[index];
-                    return Container(
-                      padding: const EdgeInsets.only(
-                          left: 12, right: 8, top: 8, bottom: 8),
-                      child: Row(
-                        mainAxisSize: MainAxisSize.max,
-                        children: [
-                          Expanded(child: Text(nameCharset.name)),
-                          const SizedBox(width: 12),
-                          Expanded(
-                            child: Text(
-                              nameCharset.newName,
-                              style: nameCharset.name != nameCharset.newName
-                                  ? TextStyle(color: Colors.blue[400])
-                                  : null,
-                            ),
-                          ),
-                          IconButton(
-                            onPressed: () => removeFile(fileList[index]),
-                            icon: const Icon(Icons.delete),
-                          ),
-                        ],
-                      ),
-                    );
-                  },
-                  itemCount: fileList.length,
-                ),
-              ),
-            ),
-          ),
-          const SizedBox(height: 12),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                onPressed: getFiles,
-                child: const Text('选择文件夹'),
-              ),
-              const SizedBox(width: 12),
-              ElevatedButton(
-                onPressed: changeNewName,
-                child: const Text('转变新名称'),
-              ),
-              const SizedBox(width: 12),
-              ElevatedButton(
-                onPressed: undoChangeName,
-                child: const Text('撤销转变'),
-              ),
-              const SizedBox(width: 12),
-              ElevatedButton(
-                onPressed: removeSameName,
-                child: const Text('删除同名项'),
-              ),
-              const SizedBox(width: 12),
-              ElevatedButton(
-                onPressed: renameDir,
-                child: const Text('确定更改'),
-              ),
-            ],
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
